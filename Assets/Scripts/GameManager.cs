ef0034251be119c6cc5e62ead5af1789fec232d3
Eduardo Tanate <eduardotanate3@gmail.com>
2022-01-23 21:59:21 +0800

    Finals

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public int lives;
+    public int level;
+
+    public GameObject player;
+
+    public GameObject panelPlay;
+    public GameObject panelGameOver;
+    private AudioSource bgMusic;
+    public GameObject panelGameWin;
+
+    public int levelIndex;
+
+    public Text livesText;
+    public Text levelText;
+
+    // Start is called before the first frame update
+    public int Lives
+    {
+        get 
+        { 
+            return lives;
+        }
+        set 
+        { 
+            lives = value; 
+            livesText.text = "Lives: " + lives;
+        }
+    }
+
+    public int Level
+    {
+        get 
+        { 
+            return level; 
+        }
+        set 
+        { 
+            level = value; 
+            levelText.text = "Level: " + level;
+        }
+    }
+ 
+
+    void Start()
+    {   
+        bgMusic = GetComponent<AudioSource>();
+
+        bgMusic.Play();
+        Lives = lives;
+        Level = level;
+        Level = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    // Update is called once per frame
+    public void GameOver() {
+        panelGameOver.SetActive(true);
+
+        bgMusic.Stop();
+        Destroy(bgMusic);
+    }
+
+    public void GameComplete() {
+        Time.timeScale = 0f;
+        panelGameWin.SetActive(true);
+    }
+}
